622dc65a9bd9dfae3f347ebb2ee7a660a118fe9d
Cuong Nguyen Manh <cuongnm4215@gmail.com>
2022-04-18 11:58:17 +0700

    Update pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,17 +6,5 @@ module.exports = {
       cwd: '/home/ubuntu/actions-runner/next-express/reward-dashboard/reward-dashboard',
       watch: false
     }
-  ],
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
-    }
-  }
+  ]
 };
--- ecosystem.staging.config.js
+++ ecosystem.staging.config.js
@@ -3,20 +3,8 @@ module.exports = {
     {
       name: 'NextExpress',
       script: 'yarn start',
-      cwd: '~/actions-runner/next-express/reward-dashboard/reward-dashboard',
+      cwd: '/home/ubuntu/actions-runner/next-express/reward-dashboard/reward-dashboard',
       watch: '.'
     }
-  ],
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
-    }
-  }
+  ]
 };
